48f0b436adfec93cbc95b16fbd587cbaca2f6bac
DmitryLiashenko <dmliash1@gmail.com>
2024-01-31 12:25:15 +0200

    Commit-1

--- GoIT-hw-4-1/hw-4-1.py
+++ GoIT-hw-4-1/hw-4-1.py
@@ -1,6 +1,11 @@
+                              # ФУНКЦЫЯ ДЛЯ РАСЧЕТА СУММЫ И СРЕДНЕЙ ЗП
 from pathlib import Path
-
+import re
 def total_salary(path = None):
-    with open('file.txt', 'r') as fh:
-        line = [el.strip() for el in fh.readlines()]
-        
+    payroll = ()
+    with open(path, 'r', encoding='utf-8') as fh:
+        lines = [el.strip() for el in fh.readlines()]
+        for line in lines:
+            pass
+    return payroll
+total_salary(r'D:\IT\Repositorys\goit-algo-hw-04\GoIT-hw-4-1\file1.txt.txt')
