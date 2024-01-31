bc98c8a98ce7dfad5ebc54a38685e4d54bf9c251
DmitryLiashenko <dmliash1@gmail.com>
2024-01-31 21:44:22 +0200

    Commit-hw-4-2-done

--- GoIT-hw-4-1/hw-4-1.py
+++ GoIT-hw-4-1/hw-4-1.py
@@ -6,6 +6,12 @@ def total_salary(path = None):
     with open(path, 'r', encoding='utf-8') as fh:
         lines = [el.strip() for el in fh.readlines()]
         for line in lines:
-            pass
+            find_digit = pass
+            int(find_digit)
+            sum = sum + find_digit
+            col = col + 1
+        sr = sum / col
+        payroll.append(sum, sr)
+    print(col)    
     return payroll
 total_salary(r'D:\IT\Repositorys\goit-algo-hw-04\GoIT-hw-4-1\file1.txt.txt')
---
+++ GoIt-hw-4-2/hw-4-2.py
@@ -0,0 +1,16 @@
+                                    # ФУНКЦИЯ СОЗДАЕТ СПИСОК СЛОВАРЕЙ С ДАННЫМИ ИЗ ФАЙЛА ЧЕРЕЗ ЗЯПЯТУЮ С ПЕРЕНОСОМ
+
+from pathlib import Path                                  # ИМПОРТИРУЕМ МЕТОД ИЗ МОДУЛЯ
+
+def get_cats_info(path = None):                           # СОЗДАЕМ ФУНКЦИЮ
+    try:                                                  # НАЧАЛО БЛОКА ОЩИБОК
+        list_cats = []                                    # СОЗДАЕМ ПУСТОЙ СПИСОК
+        with open(path, encoding='utf-8') as fh:          # ОТКРЫВАЕМ ФАЙЛ С ПОМОЩЬЮ МЕНЕДЖЕРА КОНТЕКСТОВ
+            lines = [el.strip() for el in fh.readlines()] # РАЗДЕЛЯЕМ НАКЖДУ СТРОКУ В ТЕКСТЕ
+            for line in lines:                            # ПРОХОДИМСЯ ПО ЭЛЕМЕНТАМ КАЖДОЙ СТРОКИ
+                separator = line.split(',')               # РАЗДЕЛЯЕМ ТЕКСТ ПО ПАТЕРНУ ","
+                list_cats.append({'id': separator[0], 'name' : separator[1], 'age' : separator[2]}) # ДОБАВЛЯЕМ В СЛОВАРЬ НУЖНЫЕ ДАННЫЕ
+        return list_cats                                                                            # ВОЗВРАЩАЕМ СПИСОК СЛОВАРЕЙ
+    except Exception as Eror:                                                                       # КОНЕЦ БЛОКА ОШИБОК
+        print (f'Somthing bat {Eror}')                                                              # СООБЩАЕМ ОБ ОШИБКЕ
+print(get_cats_info(r'D:\IT\Repositorys\goit-algo-hw-04\GoIt-hw-4-2\cats.txt.txt'))                 # ТЕСТ
