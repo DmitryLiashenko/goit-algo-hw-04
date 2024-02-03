411e3a2324b97582cb40f7af13ce1da29dfc3d27
DmitryLiashenko <dmliash1@gmail.com>
2024-02-03 11:29:45 +0200

    Commit-hw-4-4-start

---
+++ GoIT-hw-4-4/hw-4-4.py
@@ -0,0 +1,43 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact():
+    pass
+
+def show_phone():
+    pass
+
+def show_all():
+    pass
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            change_contact()
+        elif command == "phone":
+            show_phone()
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
