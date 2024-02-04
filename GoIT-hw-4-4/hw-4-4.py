f497d27c0fd6704cf34075c8d0a59384fc5477b9
DmitryLiashenko <dmliash1@gmail.com>
2024-02-04 14:30:25 +0200

    Commit-4-4-done

--- GoIT-hw-4-4/hw-4-4.py
+++ GoIT-hw-4-4/hw-4-4.py
@@ -1,18 +1,26 @@
-def parse_input(user_input):        #ФУНКЦИЯ ПАРСЕРА КОМАНД           
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
+def parse_input(user_input):            # ФУНКЦИЯ ПАРСЕРА КОМАНД           
+    cmd, *args = user_input.split()     # РАЗДЕЛЯЕМ ВВОД НА КОМАНДУ И АРГУМЕНТЫ
+    cmd = cmd.strip().lower()           # ПРИВОДИМ В НИЖНИЙ РЕГИСТР И УДАЛЯЕМ ЛИШНИИ ПРОБЕЛЫ
+    return cmd, *args                   # ВОЗВРАЩАЕМ КОМАНДУ И СПИСОК АРГУМЕНТОВ
 
-def add_contact(args, contacts):    # ФУНКЦИЯ ДОБАВЛЕНИЯ КОНТАКТОВ
+def add_contact(args, contacts):        # ФУНКЦИЯ ДОБАВЛЕНИЯ КОНТАКТОВ
+    name, phone = args                  # ПРИСВАИВАЕМ АРГУМЕНТЫ ИМЕНИ И ТЕЛЕФОНУ
+    contacts[name] = phone              # ЗАНОСИМ В СЛОВАРЬ ПО КЛЮЧЮ ТЕЛЕФОН
+    return "Contact added."             #
+
+def change_contact(args, contacts):     # ФУНКЦИЯ ИЗМЕНЕНИЯ КОНТАКТА
     name, phone = args
-    contacts[name] = phone
-    return "Contact added."
-
-def change_contact():
+    for key, value in contacts.items(): # ПРОХОДИМСЯ ПО СЛОВАРЮ
+        if key == name:                 # ЕСЛИ КЛЮЧ СОВПАДАЕТ С ИМЕНЕМ 
+            contacts[name] = phone      # ПРИСВАИВАЕМ ИМЕНИ НОВЫЙ ТЕЛЕФОН
     return "Contact change"
 
-def show_phone():
-    pass
+def show_phone(args, contacts):         # ФУНКЦИЯ ПОКАЗЫВАЕТ ТЕЛЕФОН ПО ИМЕНИ
+    name = args
+    for key, value in contacts.items(): 
+        if name == key:                 
+            return value                # ВОЗВРАЩАЕМ ЗНАЧЕНИЕ НАЙДЕНОЕ ПО КЛЮЧУ
+    
 
 def show_all(contacts=None):       # ФУНКЦИЯ ВЫВОДИТ ВСЕ КОНТАКТЫ В СЛОВАРЕ(ДОБАВИТЬ \n)
     return contacts
@@ -30,10 +38,10 @@ def main():
             print("How can I help you?")
         elif command == "add":                   # ДОБАВЛЕНИЕ КОНТАКТА
             print(add_contact(args, contacts))
-        elif command == "change":                # 
-             print(change_contact())
-        elif command == "phone":                 # 
-            print(show_phone())
+        elif command == "change":                # СМЕНА КОНТАКТА
+             print(change_contact(args, contacts))
+        elif command == "phone":                 # ПОКАЗЫВАЕТ КОНТАКТ ПО ИМЕНИ
+            print(show_phone(args, contacts))
         elif command == "all":                   # ПОКАЗЫВВАЕМ ВЕСЬ СЛОВАРЬ
             print(show_all(contacts)) 
         else:                                    # СООБЩАЕМ О НЕ ПРАВИЛЬНОЙ КОМАНДЕ
